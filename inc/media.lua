76ecf879364149730dca240bba5cb9591fedf7be
schme16 <schme16@d57e80e6-01d5-11df-adb7-1be7f7e3f636>
2010-10-05 15:31:52 +0000

    There a SO MANY changes in this Revision, it's nearly a different game!
    I've swapped to per-pixel collision (pPCollision.lua see file for credits)
    I'm using a sprite sheet to make levels (level editor no longer exists, but will be made some day)
    Separate files for loading media ( media.lua ), input has been reworked ( input.lua ), items have been made to collide with the player, and no longer create a game lag when spawn in number exceeding 30-ish ( items.lua )
    
    [NOTE] THE GAME HAS BEEN LEFT IN DEBUG MODE FOR THIS REV -  THIS MEANS THAT MOUSE X,Y CO-ORDS ARE SHOWN AND THE BOUNDING BOX OF THE PLAYER IS REVEALED!

---
+++ inc/media.lua
@@ -0,0 +1,9 @@
+--This is a predefined media area.
+images = 
+{
+	coin = love.graphics.newImage('img/items/coinBlock/1.png'),
+	playerColMap = love.graphics.newImage('img/player/collision.png'),
+
+
+
+}
